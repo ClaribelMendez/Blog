d46b2b4a4202f451096ebfb55904432d5e5db228
Claribel <Claribel.D.Mendez@gmail.com>
2022-05-11 04:05:39 -0400

    routes working

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-// import Blog from './components/blogposts';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Post from './components/post' 
-import Form from './components/form'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Genres from './components/genres'
-import Home from './components/home'
 import { useState, useEffect } from 'react';
-import { accessToken } from './components/spotify';
+import { accessToken, logout, getCurrentUserProfile } from './components/spotify';
 
 
 
@@ -15,9 +11,20 @@ import { accessToken } from './components/spotify';
 
 function App() {
   const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null);
 
   useEffect(() => {
     setToken(accessToken);
+    const fetchData = async () => {
+      try {
+        const { data } = await getCurrentUserProfile();
+        setProfile(data);
+      } catch(e) {
+        console.error(e);
+      }
+    };
+
+    fetchData();
   }, []);
 
 
@@ -30,21 +37,51 @@ function App() {
 //       .catch(err => console.error(err));
 //   }
 // }, []);
-  return (
-    <div>
+return (
+  <div className="App">
     <header className="App-header">
-        {!token ? (
+    {!token ? (
           <a className="App-link" href="http://localhost:8888/login">
             Log in to Spotify
           </a>
         ) : (
-          <h1>Logged in!</h1>
+          <Router>
+            <Routes>
+              <Route path="/top-artists" element={'Top Artists'}>
+                </Route>
+              </Routes>
+              <Routes>
+              <Route path="/top-tracks" element={'Top Tracks'}>
+              </Route>
+              </Routes>
+              <Routes>
+              <Route path="/playlists/:id" element={'Playlists'}>
+              </Route>
+              </Routes>
+              <Routes>
+              <Route path="/" element ={<button onClick={logout}>Log Out</button>}>
+              
+                {/* <> */}
+                  {/* <button onClick={logout}>Log Out</button> */}
+
+                  {/* {profile && (
+                    <div>
+                      <h1>{profile.display_name}</h1>
+                      <p>{profile.followers.total} Followers</p>
+                      {profile.images.length && profile.images[0].url && (
+                        <img src={profile.images[0].url} alt="Avatar"/>
+                      )}
+                    </div>
+                  )} */}
+                {/* </> */}
+              </Route>
+              </Routes>
+          </Router>
         )}
-      </header>
+    </header>
     <Genres />
-    </div>
-   
-     )
+  </div>
+);
 }
 
 export default App;
